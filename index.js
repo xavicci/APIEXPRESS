18215cde787276f2bd8166d3c7ec3c2af72b85db
Xavi Flores <xa_franc@live.com>
2024-02-09 06:44:05 -0600

    first server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("myapp");
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.listen(port, () => {
+  console.log('Estoy corriendo mi server en express ' + port);
+});
