3f572dafb82decb0c65c05293ab22aa51358f91f
Xavi Flores <xa_franc@live.com>
2024-02-13 09:05:39 -0600

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 const express = require('express');
+const cors = require('cors');
 const routeApi = require('./routes');
 const {
   errorHandler,
@@ -10,6 +11,18 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'), false);
+    }
+  },
+};
+
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express XFC jj');
